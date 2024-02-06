d2160ed2c69e2daea5779bbe4b639d8aa200245e
tree <tree@hidden.cz>
2024-02-06 11:23:46 +0100

    update

--- src/components/WeekNews.svelte
+++ src/components/WeekNews.svelte
@@ -2,6 +2,7 @@
 	import { onMount } from 'svelte';
     import yaml from 'js-yaml';
     import { marked } from 'marked';
+    import { setWeek, nextMonday, format, addDays } from 'date-fns';
 
     let data = {};
     let cats = [];
@@ -23,12 +24,22 @@
         }
 	});
 
+    const weekStart = setWeek(nextMonday(new Date(Number(year), 0, 4)), Number(week), {
+        weekStartsOn: 1,
+        firstWeekContainsDate: 4,
+    });
+    const weekEnd = addDays(weekStart, 6);
+    //const weekStartDa
+
 </script>
 
-<div class="mb-14 border border-white/20 p-6">
+<div class="mb-14 border border-white/20">
     
-    <div class="flex w-full">
-        <h1 class="text-3xl">Week {week}/{year}</h1>
+    <div class="flex w-full p-6 bg-white/10">
+        <div class="flex items-center gap-6">
+            <h1 class="text-2xl">Week {week}/{year}</h1>
+            <div class="text-xl">{format(weekStart, "MMM d")} - {format(weekEnd, "MMM d, yyyy")}</div>
+        </div>
         <div class="grow"></div>
         <div>
             <a href="https://github.com/web3privacy/news/edit/main/src/{year}/week{week}.yaml">Edit</a>
@@ -36,11 +47,10 @@
     </div>
 
     {#if data?.news}
-
-        <div>
+        <div class="p-6">
             {#each cats as cat}
-                <div>
-                    <h2 class="mt-6 mb-4 text-xl">{cat}</h2>
+                <div class="mb-6">
+                    <h2 class="mb-4 text-xl">{cat}</h2>
                     <ul class="list-disc">
                         {#each data.news.filter(n => n.cat === cat) as item}
                             <li class="ml-6 news-item">
@@ -55,11 +65,11 @@
             {/each}
         </div>
 
-        <div class="mt-6 text-sm">
+        <!--div class="mt-6 text-sm p-4 bg-white/10">
             <div>News count: {data.news?.length}</div>
             <div>Categories: {cats.join(', ')}</div>
-        </div>
+        </div-->
     {:else}
-        <div class="mt-4">Loading week ..</div>
+        <div class="mt-4 p-6">Loading week ..</div>
     {/if}
 </div>
